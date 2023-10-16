05ef52024a1169f237e71f140cceb819116bd0de
Tony Dassin M <90778349+saulktd@users.noreply.github.com>
2023-10-16 16:19:13 +0200

    First commit

--- plot.R
+++ plot.R
@@ -5,6 +5,6 @@ x2 = runif(20)
 y = sample(c(0,1), 20, prob = rep(0.5,2), replace = T)
 y = factor(y)
 
-plot(x1, x2, col = y, pch = 19, main = "a scatterplot", xlab = "x1", ylab = "x2")
+plot(x1, x2, col = "red", pch = 19, main = "a scatterplot", xlab = "x1", ylab = "x2")
 grid()
 legend("topleft", paste( "y =", levels(y)), col = seq(nlevels(y)), pch = 19, bg = "white")
